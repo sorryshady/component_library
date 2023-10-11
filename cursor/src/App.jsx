0642cda068a48e9e0a549a8f09ced8d69153d375
sorryShady <akhilnishadofficial@gmail.com>
2023-10-11 09:39:20 +0530

    first iiteration and rough implementation

--- cursor/src/App.jsx
+++ cursor/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { motion } from 'framer-motion'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  })
 
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      })
+    }
+
+    window.addEventListener('mousemove', mouseMove)
+    return () => {
+      window.removeEventListener('mousemove', mouseMove)
+    }
+  })
+  const [hovering, setHovering] = useState(false)
+  const handleMouseEnter = () => {
+    setHovering(true)
+  }
+  const handleMouseLeave = () => {
+    setHovering(false)
+  }
+  const variants = {
+    default: {
+      x: mousePosition.x,
+      y: mousePosition.y,
+    },
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className='title'
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
+        <h1>This is cursor sample project</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <motion.div
+        className={`cursor ${hovering ? 'enter' : ''}`}
+        variants={variants}
+        animate='default'
+      />
     </>
   )
 }
