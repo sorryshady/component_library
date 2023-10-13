99b79d6263fde5abcaf5b6e8ea7b73036c2238c6
sorryShady <akhilnishadofficial@gmail.com>
2023-10-13 21:35:10 +0530

    finish version one

--- navbar/src/App.jsx
+++ navbar/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import LoremIpsum from './components/LoremIpsum/LoremIpsum'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Navbar />
+      <LoremIpsum />
+    </>
+  )
 }
 
 export default App
